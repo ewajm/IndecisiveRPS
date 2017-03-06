4b282a5385376df5080e335cbc027ae83077952d
Ewa Manek <manekej@gmail.com>
2017-03-06 14:40:48 -0800

    Add splash screen for loading app

--- app/src/main/java/com/ewa/indecisiverps/ui/SplashActivity.java
+++ app/src/main/java/com/ewa/indecisiverps/ui/SplashActivity.java
@@ -1,10 +1,16 @@
 package com.ewa.indecisiverps.ui;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-/**
- * Created by Ewa on 3/6/2017.
- */
-
 public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
 }
